1d4df1f01555d575f7ef08c8fbdbf7c41f280281
Vekal28 <nikitakarpinghik@gmail.com>
2024-06-01 18:16:53 +0300

    chaging in openai_api

--- app/handlers.py
+++ app/handlers.py
@@ -1,20 +1,33 @@
 import openai
-from aiogram import F, Router
-from aiogram.types import Message
+from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import ContentType
+from aiogram.types import Message, Voice
+from app.config import settings
+import aiohttp
 
+# Инициализация роутера
 router = Router()
+
+# Установка API ключа для OpenAI
+openai.api_key = settings.openai_api_key
+
+
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer(text="Привет, я твой голосовой помощник.\nЗапиши мне голосовое сообщение с вопросом, и я тут же отвечу на него")
 
+
 async def transcribe_voice(file_path: str) -> str:
-    response = await openai.Audio.transcribe("whisper-1", file_path)
+    audio_file = open(file_path, "rb")
+    response = openai.Audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
     return response["text"]
 
+
 async def get_openai_response(text: str) -> str:
-    response = await openai.ChatCompletion.create(
+    response = await openai.ChatCompletion.acreate(
         model="gpt-4",
         messages=[
             {
@@ -25,6 +38,7 @@ async def get_openai_response(text: str) -> str:
     )
     return response.choices[0].message['content']
 
+
 async def text_to_speech(text: str) -> str:
     response = await openai.Audio.create(
         text=text,
@@ -32,13 +46,23 @@ async def text_to_speech(text: str) -> str:
         model="text-to-speech"
     )
     return response["url"]
+
+
 @router.message()
 async def handle_voice_message(message: Message):
-    voice = await message.voice.get_file()
+    voice = message.voice
+    file_info = await message.bot.get_file(voice.file_id)
+    file_path = file_info.file_path
+    file_url = f"https://api.telegram.org/file/bot{settings.tg_api_token}/{file_path}"
+    print(file_url)
 
-    file_path = f"https://api.telegram.org/file/bot%7Bsettings.telegram_api_token%7D/%7Bvoice.file_path%7D"
-    text = await transcribe_voice(file_path)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(file_url) as resp:
+            if resp.status == 200:
+                with open("voice.ogg", "wb") as f:
+                    f.write(await resp.read())
+
+    text = await transcribe_voice("voice.ogg")
     response_text = await get_openai_response(text)
     audio_url = await text_to_speech(response_text)
-    await message.reply_voice(audio_url)
-
+    await message.answer_voice(audio_url)
